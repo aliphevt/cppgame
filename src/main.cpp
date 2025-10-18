1528c68647880e117278c0a03b7336aad76f72ae
ali.porchia <alip21@vt.edu>
2025-10-18 04:15:42 -0400

    Got screen to have rectangle
    remeber to have things in while loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
 
+void makeRect() {
+    auto window = sf::RenderWindow(sf::VideoMode({ 1920u, 1080u }), "Screen");
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -15,7 +13,25 @@ int main()
             }
         }
 
-        window.clear();
+       
         window.display();
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(500, 1000));
+	rectangle.setOutlineColor(sf::Color::Red);
+	rectangle.setOutlineThickness(1000);
+	rectangle.setPosition({ 100,20 });
+
+	window.draw(rectangle);
     }
+
+
+}
+
+
+
+
+int main()
+{
+    makeRect();
+    
 }
